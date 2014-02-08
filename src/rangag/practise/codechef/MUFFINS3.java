2349c9384e289b781aa4bf58ef697e710b7ea3f3
rgovardh <ranganath.govardhanagiri@gmail.com>
2014-02-08 20:31:42 +0530

    muffin change

---
+++ src/rangag/practise/codechef/MUFFINS3.java
@@ -0,0 +1,17 @@
+/***
+ * http://www.codechef.com/problems/MUFFINS3
+ */
+package rangag.practise.codechef;
+
+import java.util.Scanner;
+
+public class MUFFINS3 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int testCases = Integer.parseInt(s.nextLine());
+		for (int i = 0; i < testCases; i++) {
+			int cups = Integer.parseInt(s.nextLine());
+			System.out.println((cups / 2) + 1);
+		}
+	}
+}
